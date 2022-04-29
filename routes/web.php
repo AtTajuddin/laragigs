3ebbac0d125b635ad507a1de61daf31d4c9551ff
anton Tajudin <anton.tajudin@gmail.com>
2022-04-29 16:42:45 +0700

    melakukan testing route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hai', function () {
+    return response('<h1> ini title</h1>', 200)
+        ->header('Content-Type', 'text plain')
+        ->header('my', 'man');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('/naon/{id}', function ($id) {
+    return response('naonwehah' . $id);
+})->where('id', '[0-9]+');
+
+Route::post('post/{id}', function ($id) {
+    return response(
+        [
+            'mypost' => [
+                'akuh' => 'ini',
+                'siapa' => 'coba',
+            ]
+        ]
+    );
+});
