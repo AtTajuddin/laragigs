bcce97e76f294880ec95d767ccdfcef365f5e914
anton Tajudin <anton.tajudin@gmail.com>
2022-05-01 17:51:40 +0700

    search Filter final

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     public static function index()
     {
         return view('listing.index', [
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
     //show single list
