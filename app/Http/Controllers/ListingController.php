d042758136d9ca49cb1be66b734e00cf0168325e
anton Tajudin <anton.tajudin@gmail.com>
2022-05-01 14:49:21 +0700

    controller -- resource naming methode

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,7 +10,7 @@ class ListingController extends Controller
     //show All List
     public static function index()
     {
-        return view('home', [
+        return view('listing.index', [
             'heading' => 'Laragigs ini pasing data from route',
             'listings' => Listing::all()
         ]);
@@ -18,7 +18,7 @@ class ListingController extends Controller
     //show single list
     public static function show(Listing $listing)
     {
-        return view('listing', [
+        return view('listing.show', [
             'listing' => $listing
         ]);
     }
